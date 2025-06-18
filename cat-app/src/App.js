5122d285b402e108ceca42152df9697594a83d94
Kitik <142086117+Kvstkk@users.noreply.github.com>
2025-06-18 19:41:48 +0300

    init react, first main page

---
+++ cat-app/src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+
+
+function App() {
+  return (
+    <div className="App">
+        <h1>Hello world.</h1>
+        <h3>What cat are you?</h3>
+        <img src="/cat.gif" alt="Cat GIF" />
+        <br />
+        <button>Find out</button>
+    </div>)
+}
+
+export default App;
