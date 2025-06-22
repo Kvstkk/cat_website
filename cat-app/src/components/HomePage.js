f7e7482dffa55386068421c5496a714094128e46
Kitik <142086117+Kvstkk@users.noreply.github.com>
2025-06-22 17:57:01 +0300

    created pages, Home page done

---
+++ cat-app/src/components/HomePage.js
@@ -0,0 +1,15 @@
+import './Homecss.css';
+
+
+function HomePage() {
+    return (
+        <div className="App">
+            <h1>Hello world.</h1>
+            <h3>What cat are you?</h3>
+            <img src="/cat.gif" alt="Cat GIF" />
+            <br />
+            <button>Find out</button>
+        </div>)
+}
+
+export default HomePage;
