70bb58eb015502cef0727b1c3008f08f22e2455c
Kitik <142086117+Kvstkk@users.noreply.github.com>
2025-06-22 19:45:35 +0300

    bind css elements to homepage div

--- cat-app/src/components/HomePage.js
+++ cat-app/src/components/HomePage.js
@@ -5,7 +5,7 @@ import { useNavigate } from 'react-router-dom';
 function HomePage() {
     const navigate = useNavigate();
     return (
-        <div className="App">
+        <div className="home-page">
             <h1>Hello world.</h1>
             <h3>What cat are you?</h3>
             <img src="/cat.gif" alt="Cat GIF" />
