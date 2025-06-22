20b53542387fa562fd11746600966fae477122b0
Kitik <142086117+Kvstkk@users.noreply.github.com>
2025-06-22 17:56:29 +0300

    react-router-dom

--- cat-app/src/App.js
+++ cat-app/src/App.js
@@ -1,15 +1,20 @@
-import './App.css';
-
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import HomePage from './components/HomePage';
+import LoginPage from './components/LoginPage';
+import SurveyPage from './components/SurveyPage';
+import ResultPage from './components/ResultPage';
 
 function App() {
-  return (
-    <div className="App">
-        <h1>Hello world.</h1>
-        <h3>What cat are you?</h3>
-        <img src="/cat.gif" alt="Cat GIF" />
-        <br />
-        <button>Find out</button>
-    </div>)
+    return (
+        <BrowserRouter>
+            <Routes>
+                <Route path="/" element={<HomePage />} />
+                <Route path="/login" element={<LoginPage />} />
+                <Route path="/page" element={<SurveyPage />} />
+                <Route path="/result" element={<ResultPage />} />
+            </Routes>
+        </BrowserRouter>
+    );
 }
 
 export default App;
