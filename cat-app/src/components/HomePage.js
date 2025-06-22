bd12b9ac0a3f6812995cefd907ad1b80a27aa950
Kitik <142086117+Kvstkk@users.noreply.github.com>
2025-06-22 17:59:45 +0300

    button on home page that send to login page

--- cat-app/src/components/HomePage.js
+++ cat-app/src/components/HomePage.js
@@ -1,14 +1,18 @@
 import './Homecss.css';
+import { useNavigate } from 'react-router-dom';
 
 
 function HomePage() {
+    const navigate = useNavigate();
     return (
         <div className="App">
             <h1>Hello world.</h1>
             <h3>What cat are you?</h3>
             <img src="/cat.gif" alt="Cat GIF" />
             <br />
-            <button>Find out</button>
+            <button onClick={() => navigate('/login')}>
+                Find out
+            </button>
         </div>)
 }
 
