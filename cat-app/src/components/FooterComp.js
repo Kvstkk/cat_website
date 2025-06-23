eaf015dbf282cc36dc9fb4fdc4d90943231cade4
Kitik <142086117+Kvstkk@users.noreply.github.com>
2025-06-23 21:57:42 +0300

    heart links to homepage

--- cat-app/src/components/FooterComp.js
+++ cat-app/src/components/FooterComp.js
@@ -1,10 +1,14 @@
 import React from 'react';
 import './Footercss.css';
+import {Link} from "react-router-dom";
+
 
 function FooterComp(){
     return(
     <div className="foot_component">
-        <img src="/heart.png" alt="heart" />
+        <Link to="/">
+            <img src="/heart.png" alt="heart" />
+        </Link>
     </div>
 )}
 export default  FooterComp;
