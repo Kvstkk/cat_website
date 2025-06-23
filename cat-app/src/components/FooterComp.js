ad80f200f7ef82f0c84cacab48bca0fc70da676e
Kitik <142086117+Kvstkk@users.noreply.github.com>
2025-06-23 21:41:14 +0300

    footer componenta careated

---
+++ cat-app/src/components/FooterComp.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import './Footercss.css';
+
+function FooterComp(){
+    return(
+    <div className="foot_component">
+        <img src="/heart.png" alt="heart" />
+    </div>
+)}
+export default  FooterComp;
