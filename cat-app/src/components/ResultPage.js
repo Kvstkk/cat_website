c723ba224ed6f98e541d73c4296a967494b1ab21
Kitik <142086117+Kvstkk@users.noreply.github.com>
2025-06-25 02:07:55 +0300

    ajax with facts

--- cat-app/src/components/ResultPage.js
+++ cat-app/src/components/ResultPage.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React,{ useEffect, useState } from 'react';
 import { useLocation } from 'react-router-dom';
 import FooterComp from './FooterComp';
 import './Resultcss.css';
@@ -7,11 +7,36 @@ function ResultPage() {
     const location = useLocation();
     const mood = location.state?.mood || 'orange';
 
+    const [fact, setFact] = useState('loading..');
+
+    async function loadFact() {
+        try {
+            const response = await fetch('https://catfact.ninja/fact');
+            const data = await response.json();
+            setFact(data.fact);
+        } catch (error) {
+            setFact('oops, error(');
+        }
+    }
+
+    useEffect(() => {
+        loadFact();
+
+        const time = setInterval(() => {
+            loadFact();
+        }, 5000);
+
+        return () => {
+            clearInterval(time);
+        };
+    }, []);
+
+
     return (
         <div className="result-page">
             <h1>Here is your cat:</h1>
             <img src={`https://cataas.com/cat/${mood}`} alt="hola" className="cat-image" />
-
+            <p>{fact}</p>
             <FooterComp />
         </div>
     );
